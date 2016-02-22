efd42f56b47ec62c68cd1d69aede263e6a24ad5a
Luke Maurits <luke@maurits.id.au>
2016-02-23 10:58:36 +1300

    Remove totally unused and untested UnicodeWriter class.

--- beastling/fileio/unicodecsv.py
+++ beastling/fileio/unicodecsv.py
@@ -31,37 +31,6 @@ class UnicodeDictReader:
         for key in row:
             newrow[unicode(key.strip(), "utf-8")] = unicode(row[key].strip(), "utf-8")
         return newrow
-        return [unicode(s, "utf-8") for s in row]
 
     def __iter__(self):
         return self
-
-class UnicodeWriter:
-    """
-    A CSV writer which will write rows to CSV file "f",
-    which is encoded in the given encoding.
-    """
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        # Redirect output to a queue
-        self.queue = cStringIO.StringIO()
-        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
-
-    def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
-        # Fetch UTF-8 output from the queue ...
-        data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        # ... and reencode it into the target encoding
-        data = self.encoder.encode(data)
-        # write to the target stream
-        self.stream.write(data)
-        # empty queue
-        self.queue.truncate(0)
-
-    def writerows(self, rows):
-        for row in rows:
-            self.writerow(row)
-
