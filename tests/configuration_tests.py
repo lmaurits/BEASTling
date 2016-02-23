cd79789d93da41174b302f3878dd1db74e08d9c9
Luke Maurits <luke@maurits.id.au>
2016-02-23 19:18:26 +1300

    Relentless push for test coverage.

--- tests/configuration_tests.py
+++ tests/configuration_tests.py
@@ -1,7 +1,10 @@
 # coding: utf8
 from __future__ import unicode_literals
 from unittest import TestCase
+import glob
 import os
+
+from nose.tools import *
 
 from beastling.configuration import Configuration
 
@@ -11,6 +14,11 @@ class Tests(TestCase):
         return Configuration(configfile=os.path.join(
             os.path.dirname(__file__), 'configs', '%s.conf' % name))
 
+    def _make_bad_cfg(self, name):
+        return Configuration(configfile=os.path.join(
+            os.path.dirname(__file__), 'configs/bad_configs/',
+            '%s.conf' % name))
+
     def test_families(self):
         cfg1 = self._make_cfg('glottolog_families')
         cfg2 = self._make_cfg('glottolog_families_from_file')
@@ -18,3 +26,33 @@ class Tests(TestCase):
         cfg2.process()
         self.assertEqual(cfg1.lang_filter, cfg2.lang_filter)
         self.assertEqual(len(cfg1.lang_filter), 6107)
+
+    @raises(ValueError)
+    def test_no_data(self):
+        cfg = self._make_bad_cfg("no_data")
+        cfg.process()
+
+    @raises(ValueError)
+    def test_no_langs(self):
+        cfg = self._make_bad_cfg("no_langs")
+        cfg.process()
+
+    @raises(ValueError)
+    def test_no_langs(self):
+        cfg = self._make_bad_cfg("no_model_sec")
+        cfg.process()
+        
+    @raises(ValueError)
+    def test_no_langs(self):
+        cfg = self._make_bad_cfg("no_model")
+        cfg.process()
+        
+    @raises(ValueError)
+    def test_no_langs(self):
+        cfg = self._make_bad_cfg("unknown_model")
+        cfg.process()
+
+    @raises(ValueError)
+    def test_no_langs(self):
+        cfg = self._make_bad_cfg("bad_overlap")
+        cfg.process()
