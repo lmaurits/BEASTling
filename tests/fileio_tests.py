c651d7f535bb536d9aa567da437991730fd67446
Gereon Kaiping <anaphory@yahoo.de>
2016-10-06 16:01:36 +0200

    Also test data readers when it's explicitly cldf

--- tests/fileio_tests.py
+++ tests/fileio_tests.py
@@ -19,11 +19,21 @@ class Tests(TestCase):
     def test(self):
         beastling_format = load_data(data_path("basic.csv"))
         cldf_format = load_data(data_path("cldf.csv"))
+        explicit_cldf_format = load_data(data_path("cldf.csv"),
+                                file_format='cldf')
         tabbed_cldf_format = load_data(data_path("cldf.tsv"))
+        tabbed_explicit_cldf_format = load_data(data_path("cldf.tsv"),
+                                                file_format='cldf')
         assert set(list(beastling_format.keys())) == set(list(cldf_format.keys()))
+        assert set(list(beastling_format.keys())) == set(list(explicit_cldf_format.keys()))
         assert set(list(beastling_format.keys())) == set(list(tabbed_cldf_format.keys()))
+        assert set(list(beastling_format.keys())) == set(list(tabbed_explicit_cldf_format.keys()))
         for key in beastling_format:
             self.assertEqual(
                 set(beastling_format[key].items()), set(cldf_format[key].items()))
             self.assertEqual(
+                set(beastling_format[key].items()), set(explicit_cldf_format[key].items()))
+            self.assertEqual(
                 set(beastling_format[key].items()), set(tabbed_cldf_format[key].items()))
+            self.assertEqual(
+                set(beastling_format[key].items()), set(tabbed_explicit_cldf_format[key].items()))
