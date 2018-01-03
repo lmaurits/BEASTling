75b9989b189d6e13e2940089478aa8d393230192
Luke Maurits <luke@maurits.id.au>
2018-01-03 15:17:38 +0200

    Adapt to the disappearance of clldutils.testing by simply importing the little bit of code we used into tests/util.py.

--- tests/stdout_test.py
+++ tests/stdout_test.py
@@ -2,11 +2,11 @@ from __future__ import unicode_literals
 
 import beastling.beastxml
 import beastling.configuration
-from .util import WithConfigAndTempDir, capture, config_path
+from .util import WithConfigAndTempDir, old_capture, config_path
 
 
 class Tests(WithConfigAndTempDir):
     def test_stdout(self):
         xml = beastling.beastxml.BeastXml(self.make_cfg(config_path("basic").as_posix()))
-        with capture(xml.write_file, 'stdout') as output:
+        with old_capture(xml.write_file, 'stdout') as output:
             self.assertIn('<?xml version=', output[0].decode('utf8'))
