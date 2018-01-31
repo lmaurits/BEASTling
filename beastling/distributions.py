3be61d5910fa1a0df235231c1c7bb59ad112ab8b
Gereon A. Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2018-01-31 19:59:15 +0100

    Start working on a dedicated distribution module.
    Because of #119, start by moving the set of map-registered
    distributions to the new module.

---
+++ beastling/distributions.py
@@ -0,0 +1,11 @@
+registered_distributions = (
+    ("Beta", "beast.math.distributions.Beta"),
+    ("Exponential", "beast.math.distributions.Exponential"),
+    ("InverseGamma", "beast.math.distributions.InverseGamma"),
+    ("LogNormal", "beast.math.distributions.LogNormalDistributionModel"),
+    ("Gamma", "beast.math.distributions.Gamma"),
+    ("Uniform", "beast.math.distributions.Uniform"),
+    ("LaplaceDistribution", "beast.math.distributions.LaplaceDistribution"),
+    ("OneOnX", "beast.math.distributions.OneOnX"),
+    ("Normal", "beast.math.distributions.Normal"),
+)
