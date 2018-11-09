316aa137509c8bc652de2d1380bec0ee1d133ec8
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2018-11-09 16:40:37 +0100

    Wrap errors only when called from the command line.
    Do not catch-and-sys.exit errors when called from `python -m`, so that the it is
    easier to debug (using pdb.pm(), eg.) errors from calling Beastling from the
    command line.
    
    This is achieved by defining which errors should be caught and turned into
    various exit codes using `sys.exit()` – the default is that all exceptions be
    caught, but `__main__` re-defines that variable to a class that is never raised,
    and therefore does not catch most exceptions.
    
    I am sure there is a better way to run this, and I would be happy to see it
    implemented, but I have no intuition what it might be.

--- beastling/__main__.py
+++ beastling/__main__.py
@@ -2,6 +2,13 @@
 from __future__ import unicode_literals
 from beastling.cli import main
 
+import beastling.cli
+
+class NoneException (Exception):
+    pass
+
+beastling.cli.wrap_errors = NoneException
+
 
 if __name__ == "__main__":
     main()
