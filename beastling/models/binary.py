8db8340495c0e3a7419be433fac7206180351a29
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2019-01-18 15:00:46 +0100

    Something had gone wrong with '?'.
    TODO: Make a regression test for this

--- beastling/models/binary.py
+++ beastling/models/binary.py
@@ -216,14 +216,21 @@ class BinaryModel(BaseModel):
                 # need to add one "all zeros" column to account for the recoding
                 extra_columns = ["0"]
             self.extracolumns[feature] = extra_columns
+
+            if "?" in point:
+                # TODO: THIS IS A HACK AND NOT WHERE THIS SHOULD BE HANDLED, BUT I NEED A FIX NOW
+                self.feature_has_unknown_values[feature] = True
+                point.remove("?")
+
             # Start with all zeros/question marks
             if self.feature_has_unknown_values[feature]:
                 absent = "?"
             else:
                 absent = "0"
 
-                valuestring = extra_columns + [
-                    absent for i in range(0, self.valuecounts[feature])]
+            valuestring = extra_columns + [
+                absent for i in range(0, self.valuecounts[feature])]
+
             # Set the appropriate data column to 1
             for subpoint in point:
                 if subpoint == "?":
