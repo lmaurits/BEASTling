2ed692aeed1fd56f0990efe47021202a4336102c
Luke Maurits <luke@maurits.id.au>
2019-04-15 13:57:04 +0300

    Install the treepriors package along with everything else.  Feels very wrong that these need to be manually declared one-by-one, but oh well.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'License :: OSI Approved :: BSD License',
     ],
-    packages=['beastling','beastling.clocks','beastling.fileio','beastling.models'],
+    packages=['beastling','beastling.clocks','beastling.fileio','beastling.models','beastling.treepriors'],
     install_requires=requires,
     tests_require=['mock==1.0.0', 'nose'],
     entry_points={
