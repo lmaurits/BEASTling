0ca2dc9d8399e15a6f8b3b1dc19df784408a8be4
Luke Maurits <luke@maurits.id.au>
2019-07-27 22:30:36 +0300

    Add test for gamma-distributed rate heterogeneity.

--- tests/beastrun_tests.py
+++ tests/beastrun_tests.py
@@ -49,6 +49,7 @@ def test_basic():
         ("admin", "bsvs"),
         ("admin", "mk", "strictclockwithprior"),
         ("admin", "binaryctmc"),
+        ("admin", "binaryctmc", "gamma_categories"),
         ("admin", "covarion_multistate"),
         ("admin", "covarion_multistate", "covarion_per_feature_params"),
         ("admin", "covarion_multistate", "ascertainment_true"),
