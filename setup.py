3780c8257ed7e603f841f46a671440aca0dbbc26
Luke Maurits <luke@maurits.id.au>
2019-07-29 19:57:17 +0300

    So long, 2.7, and thanks for all the spam!  Closes #229

--- setup.py
+++ setup.py
@@ -25,8 +25,6 @@ setup(
     license="BSD (3 clause)",
     classifiers=[
         'Programming Language :: Python',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
